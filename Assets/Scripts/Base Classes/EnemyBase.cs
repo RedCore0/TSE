ddc5ae9716dc9cc6a742b5e2ab0806942378b853
Leon Stokes <4013lest03@gmail.com>
2024-02-22 11:22:29 +0000

    Update EnemyBase.cs

--- Assets/Scripts/Base Classes/EnemyBase.cs
+++ Assets/Scripts/Base Classes/EnemyBase.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class EnemyBase : MonoBehaviour
 {
@@ -15,21 +17,48 @@ public class EnemyBase : MonoBehaviour
     public float speed;
 
     [Tooltip ("Damage dealt to structure")]
-    public int damage;  
-//  public string element;  // if we use it, will probably increase damage by up to 2x if weak, and decrease by 0.5x if resistent
-                            // could be implemented in one of two ways:
-                            // a system where the selected element(s) are what the enemy is weak to
-                            // or a system where the selected element is what the enemy is 'attuned to', similar to pokemon, and this element is weak/strong against others
+    public int damage;
+
+    //public float range        // range of enemy, if we use it
+
+    //  public string element;  // if we use it, will probably increase damage by up to 2x if weak, and decrease by 0.5x if resistent
+                                // could be implemented in one of two ways:
+                                // a system where the selected element(s) are what the enemy is weak to
+                                // or a system where the selected element is what the enemy is 'attuned to', similar to pokemon, and this element is weak/strong against others
+
+
+    private GameObject enemyPath = GameObject.Find("Path");     // finds and stores a reference to the path object
+    private GameObject[] pathNodes;
+    private int nextNode = 0;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pathNodes = enemyPath.GetComponents<GameObject>();      // places all nodes on the path into an array
     }
 
     // Fixed Update is called once per fixed interval
     void FixedUpdate()
     {
-        // move by x * speed
+        if(Vector2.Distance(this.transform.position, pathNodes[nextNode].transform.position) > 0)
+        {
+            if(Vector2.SignedAngle(this.transform.position, pathNodes[nextNode].transform.position) != 0)
+            this.transform.rotation = Vector2.SignedAngle(this.transform.position, pathNodes[nextNode].transform.position);
+                       
+            // move by x * speed
+        }
+        else if(nextNode++ < pathNodes.Length-1) 
+        {
+            nextNode++;
+        }
+        else //if range > distance to endnode
+        {
+            Attack();
+        }
+    }
+
+    public virtual void Attack()
+    {
+        // basic attack script if necessary, but will probably be overwritten per enemy
     }
 }
